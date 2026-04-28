309bc9d965e5e61de3dca2c428eb786e93a87119
Moritz2k1 <moritz.gutschi@outlook.com>
2026-04-28 20:14:29 +0200

    docs: Removed redundant documentation of domain models to README.md

--- src/main/kotlin/org/machikoro/server/domain/models/PlayerModel.kt
+++ src/main/kotlin/org/machikoro/server/domain/models/PlayerModel.kt
@@ -1,13 +1,5 @@
 package org.machikoro.server.domain.models
 
-/**
- * Domain model representing data used in the application's core logic
- * - Encapsulates pure business data independent of persistence and frameworks
- * - Used by the service layer to implement game logic
- * - Can be used for DTO's
- * - Does not contain database logic
- * - Used to represent the game state
- */
 data class PlayerModel(
     val id: Int,
     val gameId: Int,
