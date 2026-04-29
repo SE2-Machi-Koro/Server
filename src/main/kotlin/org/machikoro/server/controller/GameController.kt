dbc2aed605667b8e6dd7f8f5a02b67bba8d7c3d8
doniliks <lavrovdanylo@gmail.com>
2026-04-29 13:13:58 +0200

    refactor: adjust current functions to new return value of service and broadcast data based on return value

--- src/main/kotlin/org/machikoro/server/controller/GameController.kt
+++ src/main/kotlin/org/machikoro/server/controller/GameController.kt
@@ -1,5 +1,6 @@
 package org.machikoro.server.controller
 
+import org.machikoro.server.domain.enums.TurnPhase
 import org.machikoro.server.domain.models.PlayerModel
 import org.machikoro.server.dto.EndTurnRequest
 import org.machikoro.server.dto.LeaveFinishedGameRequest
@@ -9,6 +10,7 @@ import org.machikoro.server.dto.RollDiceRequest
 import org.machikoro.server.dto.WebSocketMessage
 import org.machikoro.server.service.DiceService
 import org.machikoro.server.service.GamePhaseService
+import org.machikoro.server.service.GamePhaseService.EndTurnOutcome
 import org.machikoro.server.service.LeaveFinishedGameService
 import org.machikoro.server.service.PurchaseResult
 import org.machikoro.server.service.PurchaseService
@@ -42,9 +44,16 @@ class GameController(
 
     @MessageMapping("/game.endTurn")
     fun endTurn(@Payload request: EndTurnRequest) {
-        val newPhase = gamePhaseService.endTurn(request.gameId)
-        logger.info("Ended turn for game ${request.gameId}, new phase $newPhase")
-        broadcastPhase(request.gameId, newPhase)
+        when (val result = gamePhaseService.endTurn(request.gameId)) {
+            is EndTurnOutcome.Continue -> {
+                logger.info("Ended turn for game ${request.gameId}, new phase ${result.nextPhase}")
+                broadcastPhase(request.gameId, result.nextPhase)
+            }
+            is EndTurnOutcome.Won -> {
+                logger.info("Game ${request.gameId} finished, winner=${result.winner.id}")
+                broadcastWinner(request.gameId, result.winner)
+            }
+        }
     }
 
     @MessageMapping("/game.leave")
@@ -86,7 +95,7 @@ class GameController(
         }
     }
 
-    private fun broadcastPhase(gameId: Int, ) {
+    private fun broadcastPhase(gameId: Int, newPhase : TurnPhase) {
         messagingTemplate.convertAndSend(
             "/topic/game/$gameId",
             WebSocketMessage(
