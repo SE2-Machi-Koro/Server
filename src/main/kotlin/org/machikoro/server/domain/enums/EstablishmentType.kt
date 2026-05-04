f7a1686f5f49926543cefbd6436865fd025418c6
doniliks <lavrovdanylo@gmail.com>
2026-05-04 17:08:05 +0200

    test: add missing EstablishmentType

---
+++ src/main/kotlin/org/machikoro/server/domain/enums/EstablishmentType.kt
@@ -0,0 +1,16 @@
+package org.machikoro.server.domain.enums
+
+enum class EstablishmentType {
+    AGRICULTURE,
+    LIVESTOCK,
+    RESOURCE,
+    RETAIL,
+    INDUSTRY,
+    HOSPITALITY,
+    MAJOR,
+    // for later expansion, tbd if necessary
+    MARITIME,
+    ENTERPRISE,
+    // not usable in current DB-setup, tbd if necessary - remove or change setup
+    LANDMARK
+}
