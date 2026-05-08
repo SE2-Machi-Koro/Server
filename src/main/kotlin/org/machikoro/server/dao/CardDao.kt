22b952f9fce7b7afce1755e625890c1418f7086a
Moritz2k1 <moritz.gutschi@outlook.com>
2026-05-08 21:15:22 +0200

    feat: Implemented and adjusted the CardDao, CardActivationNumbersDao.kt and CardModel

--- src/main/kotlin/org/machikoro/server/dao/CardDao.kt
+++ src/main/kotlin/org/machikoro/server/dao/CardDao.kt
@@ -2,6 +2,7 @@ package org.machikoro.server.dao
 
 import org.jetbrains.exposed.v1.core.ResultRow
 import org.jetbrains.exposed.v1.core.eq
+import org.jetbrains.exposed.v1.core.inList
 import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.machikoro.server.database.Cards
@@ -10,16 +11,25 @@ import org.machikoro.server.domain.models.CardModel
 import org.springframework.stereotype.Repository
 
 @Repository
-class CardDao {
+class CardDao(
+    private val cardActivationNumbersDao: CardActivationNumbersDao
+) {
 
-    private fun ResultRow.toModel() = CardModel(
-        id = this[Cards.id].value,
-        cardType = this[Cards.cardType],
-        cost = this[Cards.cost],
-        diceMin = this[Cards.diceMin],
-        diceMax = this[Cards.diceMax],
-        income = this[Cards.income]
-    )
+    private fun ResultRow.toModel(): CardModel {
+        val cardId = this[Cards.id].value
+        val activationNumbers = cardActivationNumbersDao.findByCardId(cardId)
+
+        return CardModel(
+            id = cardId,
+            cardType = this[Cards.cardType],
+            cost = this[Cards.cost],
+            income = this[Cards.income],
+            establishmentType = this[Cards.establishmentType],
+            triggerCondition = this[Cards.triggerCondition],
+            paymentSource = this[Cards.paymentSource],
+            activationNumbers = activationNumbers
+        )
+    }
 
     /**
      * Finds all available card definitions from database
@@ -37,5 +47,17 @@ class CardDao {
             .where { Cards.cardType eq cardType }
             .singleOrNull()
             ?.toModel()
+    }
+
+    /**
+     * Find cards that activate on a specific dice number
+     * @param diceNumber The dice roll to check
+     * @return List of cards that activate on this number
+     */
+    fun findByActivationNumber(diceNumber: Int): List<CardModel> = transaction {
+        val cardIds = cardActivationNumbersDao.findCardsByActivationNumber(diceNumber)
+        Cards.selectAll()
+            .where { Cards.id inList cardIds }
+            .map { it.toModel() }
     }
 }
--- src/main/kotlin/org/machikoro/server/domain/models/CardModel.kt
+++ src/main/kotlin/org/machikoro/server/domain/models/CardModel.kt
@@ -1,12 +1,28 @@
 package org.machikoro.server.domain.models
 
 import org.machikoro.server.domain.enums.CardType
+import org.machikoro.server.domain.enums.EstablishmentType
+import org.machikoro.server.domain.enums.TriggerCondition
+import org.machikoro.server.domain.enums.PaymentSource
 
+/**
+ * Represents a card definition with all its properties
+ * @property id The database ID of the card
+ * @property cardType The unique card type (WHEAT_FIELD, BAKERY, etc.)
+ * @property cost The coin cost to purchase this card
+ * @property income The coin income generated when activated
+ * @property establishmentType The card symbol/icon (WHEAT, BREAD, CUP, etc.)
+ * @property triggerCondition When the card activates (OWN_TURN, ANY_TURN, OTHER_TURN, NONE)
+ * @property paymentSource Where payment comes from (BANK, ACTIVE_PLAYER, ALL_PLAYERS)
+ * @property activationNumbers The dice numbers that trigger this card (queried separately)
+ */
 data class CardModel(
     val id: Int,
     val cardType: CardType,
     val cost: Int,
-    val diceMin: Int,
-    val diceMax: Int,
-    val income: Int
+    val income: Int,
+    val establishmentType: EstablishmentType?,
+    val triggerCondition: TriggerCondition?,
+    val paymentSource: PaymentSource?,
+    val activationNumbers: List<Int> = emptyList()
 )
