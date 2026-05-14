c7074df99062381f03a1c79cad02f33d29f6eaa6
SoupCube <66270931+SoupCube@users.noreply.github.com>
2026-05-14 14:46:14 +0200

    Create initialization service
    only vague skeleton

---
+++ src/main/kotlin/org/machikoro/server/service/InitializationService.kt
@@ -0,0 +1,21 @@
+package org.machikoro.server.service
+
+class InitializationService {
+
+    //lock input
+    fun lockInput(){
+
+    }
+    //determine player order
+    fun generateOrder(){
+
+    }
+    //initialize resources
+    fun generateResources(){
+
+    }
+    //trigger first players turn
+    fun startFirstTurn(){
+        //unlock input
+    }
+}
