cfb21498b09e924115a0845ab9dae730310be382
SoupCube <66270931+SoupCube@users.noreply.github.com>
2026-05-15 11:16:45 +0200

    Initialize Random Player Order

--- src/main/kotlin/org/machikoro/server/service/InitializationService.kt
+++ src/main/kotlin/org/machikoro/server/service/InitializationService.kt
@@ -1,21 +1,47 @@
 package org.machikoro.server.service
 
-class InitializationService {
+import org.machikoro.server.dao.GameDao
+import org.machikoro.server.dao.PlayerDao
+
+/**
+ * Service responsible for creating the initial game state
+ * Handles the player order, distribution of initial resources.
+ *
+ * Machi Koro start resources per player: 3 coins, 2 establishments, 4 deactivated landmarks
+ */
+
+class InitializationService (
+    private val playerDao: PlayerDao,
+    private val gameDao: GameDao,
+) {
+
+    fun initialize(gameId: Int) {
+        generateRandomOrder(gameId)
+    }
 
     //lock input
-    fun lockInput(){
+    private fun lockInput(){
 
     }
     //determine player order
-    fun generateOrder(){
+    private fun generateRandomOrder(gameId: Int){
+        val game = gameDao.findById(gameId)
+
+        //get list of players and shuffle
+        val players = playerDao.getPlayers(gameId).shuffled()
+        //assign shuffled position as turn order position
+        players.forEachIndexed { index, player ->
+            playerDao.updateTurnOrder(player.id, index)
+        }
 
     }
     //initialize resources
-    fun generateResources(){
+    private fun generateResources(){
 
     }
     //trigger first players turn
-    fun startFirstTurn(){
+    private fun startFirstTurn(){
         //unlock input
     }
+
 }
