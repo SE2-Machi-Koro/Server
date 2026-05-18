7dd4518412f4b837701e222cd1e7d47aa4632f8f
Moritz2k1 <moritz.gutschi@outlook.com>
2026-05-18 16:33:21 +0200

    fix: Added proper formatting

--- src/main/kotlin/org/machikoro/server/database/DataSeeder.kt
+++ src/main/kotlin/org/machikoro/server/database/DataSeeder.kt
@@ -37,21 +37,101 @@ class DataSeeder : CommandLineRunner {
         )
 
         val all = listOf(
-            CardSeed(CardType.WHEAT_FIELD, 1, 1, CardColor.BLUE, EstablishmentType.WHEAT, PaymentSource.BANK, listOf(1)),
+            CardSeed(
+                CardType.WHEAT_FIELD,
+                1,
+                1,
+                CardColor.BLUE,
+                EstablishmentType.WHEAT,
+                PaymentSource.BANK,
+                listOf(1)
+            ),
             CardSeed(CardType.RANCH, 1, 1, CardColor.BLUE, EstablishmentType.COW, PaymentSource.BANK, listOf(2)),
             CardSeed(CardType.FOREST, 3, 1, CardColor.BLUE, EstablishmentType.GEAR, PaymentSource.BANK, listOf(5)),
             CardSeed(CardType.MINE, 6, 5, CardColor.BLUE, EstablishmentType.GEAR, PaymentSource.BANK, listOf(9)),
-            CardSeed(CardType.APPLE_ORCHARD, 3, 3, CardColor.BLUE, EstablishmentType.WHEAT, PaymentSource.BANK, listOf(10)),
+            CardSeed(
+                CardType.APPLE_ORCHARD,
+                3,
+                3,
+                CardColor.BLUE,
+                EstablishmentType.WHEAT,
+                PaymentSource.BANK,
+                listOf(10)
+            ),
             CardSeed(CardType.BAKERY, 1, 1, CardColor.GREEN, EstablishmentType.BREAD, PaymentSource.BANK, listOf(2, 3)),
-            CardSeed(CardType.CONVENIENCE_STORE, 2, 3, CardColor.GREEN, EstablishmentType.BREAD, PaymentSource.BANK, listOf(4)),
-            CardSeed(CardType.CHEESE_FACTORY, 5, 3, CardColor.GREEN, EstablishmentType.FACTORY, PaymentSource.BANK, listOf(7)),
-            CardSeed(CardType.FURNITURE_FACTORY, 3, 3, CardColor.GREEN, EstablishmentType.FACTORY, PaymentSource.BANK, listOf(8)),
-            CardSeed(CardType.FRUIT_AND_VEGETABLE_MARKET, 2, 2, CardColor.GREEN, EstablishmentType.FRUIT, PaymentSource.BANK, listOf(11, 12)),
+            CardSeed(
+                CardType.CONVENIENCE_STORE,
+                2,
+                3,
+                CardColor.GREEN,
+                EstablishmentType.BREAD,
+                PaymentSource.BANK,
+                listOf(4)
+            ),
+            CardSeed(
+                CardType.CHEESE_FACTORY,
+                5,
+                3,
+                CardColor.GREEN,
+                EstablishmentType.FACTORY,
+                PaymentSource.BANK,
+                listOf(7)
+            ),
+            CardSeed(
+                CardType.FURNITURE_FACTORY,
+                3,
+                3,
+                CardColor.GREEN,
+                EstablishmentType.FACTORY,
+                PaymentSource.BANK,
+                listOf(8)
+            ),
+            CardSeed(
+                CardType.FRUIT_AND_VEGETABLE_MARKET,
+                2,
+                2,
+                CardColor.GREEN,
+                EstablishmentType.FRUIT,
+                PaymentSource.BANK,
+                listOf(11, 12)
+            ),
             CardSeed(CardType.CAFE, 2, 1, CardColor.RED, EstablishmentType.CUP, PaymentSource.ACTIVE_PLAYER, listOf(3)),
-            CardSeed(CardType.FAMILY_RESTAURANT, 3, 2, CardColor.RED, EstablishmentType.CUP, PaymentSource.ACTIVE_PLAYER, listOf(9, 10)),
-            CardSeed(CardType.STADIUM, 6, 2, CardColor.PURPLE, EstablishmentType.MAJOR, PaymentSource.ALL_PLAYERS, listOf(6)),
-            CardSeed(CardType.TV_STATION, 7, 5, CardColor.PURPLE, EstablishmentType.MAJOR, PaymentSource.CHOSEN_PLAYER, listOf(6)),
-            CardSeed(CardType.BUSINESS_CENTER, 8, 0, CardColor.PURPLE, EstablishmentType.MAJOR, PaymentSource.CHOSEN_PLAYER, listOf(6)),
+            CardSeed(
+                CardType.FAMILY_RESTAURANT,
+                3,
+                2,
+                CardColor.RED,
+                EstablishmentType.CUP,
+                PaymentSource.ACTIVE_PLAYER,
+                listOf(9, 10)
+            ),
+            CardSeed(
+                CardType.STADIUM,
+                6,
+                2,
+                CardColor.PURPLE,
+                EstablishmentType.MAJOR,
+                PaymentSource.ALL_PLAYERS,
+                listOf(6)
+            ),
+            CardSeed(
+                CardType.TV_STATION,
+                7,
+                5,
+                CardColor.PURPLE,
+                EstablishmentType.MAJOR,
+                PaymentSource.CHOSEN_PLAYER,
+                listOf(6)
+            ),
+            CardSeed(
+                CardType.BUSINESS_CENTER,
+                8,
+                0,
+                CardColor.PURPLE,
+                EstablishmentType.MAJOR,
+                PaymentSource.CHOSEN_PLAYER,
+                listOf(6)
+            ),
         )
 
         all.forEach { seed ->
