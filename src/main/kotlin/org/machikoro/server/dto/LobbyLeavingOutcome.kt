d3c6fe93f432f6ff73c47e337ab4500aa10c69f4
doniliks <lavrovdanylo@gmail.com>
2026-05-22 18:46:15 +0200

    feat: add new LeaveLobbyOutcome for lobby leaving result

---
+++ src/main/kotlin/org/machikoro/server/dto/LobbyLeavingOutcome.kt
@@ -0,0 +1,24 @@
+package org.machikoro.server.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(
+    description = "Result of leaving lobby",
+    oneOf = [LobbyLeavingOutcome.LobbyRemains::class, LobbyLeavingOutcome.LobbyDeleted::class]
+)
+sealed interface LobbyLeavingOutcome {
+
+    @Schema(description = "Normal player left the game, lobby remains for other players")
+    data class LobbyRemains(
+        @field:Schema(description = "ID of the game to notify player leaving", example = "2")
+        val gameId: Int,
+        @field:Schema(description = "ID of the user leaving", example = "12")
+        val userId: Int
+    ) : LobbyLeavingOutcome
+
+    @Schema(description = "Host left the game, lobby was deleted")
+    data class LobbyDeleted(
+        @field:Schema(description = "ID of the game to close lobby for", example = "1")
+        val gameId: Int
+    ) : LobbyLeavingOutcome
+}
